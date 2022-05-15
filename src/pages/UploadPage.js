5fd2eebfcd9228a5736c0072425df0faa4c27abd
clement-gambier2 <78157383+clement-gambier2@users.noreply.github.com>
2022-05-15 18:42:49 +0200

    progress bar

--- src/pages/UploadPage.js
+++ src/pages/UploadPage.js
@@ -1,10 +1,21 @@
 import "../style/index.css";
+import "../style/UploadPage.css";
 function UploadPage(props){
+    const setState = props.setter;
+
+    const animation = () => {
+
+        // setTimeout(3000)
+        setState("final")
+        
+    };
+
     return(
-        <main>
+        <main onLoad={animation}>
             <h1>Uploading</h1>
-            <span id="loading-bar"></span>
-            <p>{props.link}</p>
+            <div id="loading-bar">
+                <span id="progress-value"></span>
+            </div>
         </main>
     );
 }
